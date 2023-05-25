05911227f4950f05688a0fed86c2d2feca869bfc
UChicagoPhysicsLabs <55201443+UChicagoPhysicsLabs@users.noreply.github.com>
2023-05-25 15:54:55 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,8 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Red Pitaya PET Experiment'
+author = 'Kevin Van De Bogart'
 
 release = '0.1'
 version = '0.1.0'
