77b85007557c072556e250867d38ac6b15590944
UChicagoPhysicsLabs <55201443+UChicagoPhysicsLabs@users.noreply.github.com>
2023-05-26 10:02:11 -0500

    css tweaks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,3 +32,13 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+# Stolen from https://stackoverflow.com/questions/69359978/grid-table-does-not-word-wrap
+# These folders are copied to the documentation's HTML output
+#html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
